de0b7534f580bb222eaa4fb2126f3ada241257f0
Andrew Bruce <abruce1711@gmail.com>
2020-10-06 14:27:35 +0100

    Moved index to properties controller and made it the default

--- PropertyLettingsPortal/Controllers/HomeController.cs
+++ PropertyLettingsPortal/Controllers/HomeController.cs
@@ -3,44 +3,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using PropertyLettingsPortal.Data.Models;
-using PropertyLettingsPortal.Interfaces;
 using PropertyLettingsPortal.Models;
 
 namespace PropertyLettingsPortal.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IPropertyService _properties;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, IPropertyService properties)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _properties = properties;
-        }
-
-        private PropertyIndexViewModel ConvertToViewModel(IEnumerable<Property> properties)
-        {
-            var propertyModelList = properties.Select(property => new PropertyIndexListViewModel
-            {
-                Id = property.Id,
-                StreetAddress = property.StreetAddress,
-                City = property.City,
-                Postcode = property.Postcode,
-                CostPM = property.CostPM
-            });
-
-            var model = new PropertyIndexViewModel() { Properties = propertyModelList };
-            return model;
         }
 
         public IActionResult Index()
         {
-            return View(ConvertToViewModel(_properties.GetAll()));
+            return View();
         }
 
         public IActionResult Privacy()
---
+++ PropertyLettingsPortal/Controllers/PropertiesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PropertyLettingsPortal.Data.Models;
+using PropertyLettingsPortal.Interfaces;
+using PropertyLettingsPortal.Models;
+
+namespace PropertyLettingsPortal.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        private readonly IPropertyService _properties;
+        private readonly ILogger<PropertiesController> _logger;
+
+        public PropertiesController(ILogger<PropertiesController> logger, IPropertyService properties)
+        {
+            _logger = logger;
+            _properties = properties;
+        }
+
+        // Retrieves list of all properties and converts them to index view models for listing on home page
+        private PropertyIndexViewModel ConvertToViewModel(IEnumerable<Property> properties)
+        {
+            var propertyModelList = properties.Select(property => new PropertyIndexListViewModel
+            {
+                Id = property.Id,
+                StreetAddress = property.StreetAddress,
+                City = property.City,
+                Postcode = property.Postcode,
+                CostPM = property.CostPM
+            });
+
+            var model = new PropertyIndexViewModel() { Properties = propertyModelList };
+            return model;
+        }
+
+        public IActionResult Index()
+        {
+            return View(ConvertToViewModel(_properties.GetAll()));
+        }
+    }
+}
