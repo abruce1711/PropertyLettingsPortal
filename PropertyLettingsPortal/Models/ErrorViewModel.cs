2cafdd9e964e02e548d548ea485874625e57fb95
Andrew Bruce <abruce1711@gmail.com>
2020-10-06 10:17:42 +0100

    Init commit, created project

---
+++ PropertyLettingsPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PropertyLettingsPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
