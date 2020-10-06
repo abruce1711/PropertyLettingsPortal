011360d2757f01fe38065da8472a0a02fdb23684
Andrew Bruce <abruce1711@gmail.com>
2020-10-06 12:23:00 +0100

    Added migrations

--- PropertyLettingsPortal/Data/ApplicationDbContext.cs
+++ PropertyLettingsPortal/Data/ApplicationDbContext.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PropertyLettingsPortal.Data.Models;
 
 namespace PropertyLettingsPortal.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        //Dbsets representing tables
+        public DbSet<Property> Property { get; set; }
+        public DbSet<PropertyManager> PropertyManager { get; set; }
     }
 }
---
+++ PropertyLettingsPortal/Data/Migrations/20201006112236_addPropertyAndManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PropertyLettingsPortal.Data.Migrations
+{
+    public partial class addPropertyAndManager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PropertyManager",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyManager", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Property",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ManagerId = table.Column<int>(nullable: false),
+                    StreetAddress = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Postcode = table.Column<string>(nullable: true),
+                    CostPM = table.Column<float>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Available = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Property", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Property_PropertyManager_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "PropertyManager",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_ManagerId",
+                table: "Property",
+                column: "ManagerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Property");
+
+            migrationBuilder.DropTable(
+                name: "PropertyManager");
+        }
+    }
+}
