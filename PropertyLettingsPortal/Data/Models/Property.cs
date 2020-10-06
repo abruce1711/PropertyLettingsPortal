155e607a73d6eabda3df1bda3fd0818569a91cea
Andrew Bruce <abruce1711@gmail.com>
2020-10-06 12:17:28 +0100

    Added property and manager models

---
+++ PropertyLettingsPortal/Data/Models/Property.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyLettingsPortal.Data.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public int ManagerId { get; set; }
+
+        [ForeignKey("ManagerId")]
+        public PropertyManager Manager { get; set; }
+
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public float CostPM { get; set; }
+        public string Description { get; set; }
+        public DateTime Available { get; set; }
+    }
+}
---
+++ PropertyLettingsPortal/Data/Models/PropertyManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyLettingsPortal.Data.Models
+{
+    public class PropertyManager
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
